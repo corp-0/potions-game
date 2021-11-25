94943bb0e18d3932a6c76dd517b9fc53a685d1fe
Andrés Riquelme <andresfranco.rs@gmail.com>
2021-11-25 00:18:27 -0300

    feat: first iteration of player movement and aiming

---
+++ potions-game/src/Player/Direction.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace PotionsGame.Player
+{
+    public enum Direction
+    {
+        Up,
+        Down,
+        Left,
+        Right
+    }
+
+    public static class DirectionExtensions
+    {
+        public static string ToMoveControlString(this Direction direction)
+        {
+            return direction switch
+            {
+                Direction.Up => "move_up",
+                Direction.Down => "move_down",
+                Direction.Left => "move_left",
+                Direction.Right => "move_right",
+                _ => throw new ArgumentOutOfRangeException(nameof(direction), direction,
+                    $"Received not supported direction for movement {direction}")
+            };
+        }
+
+        public static string ToAimControlString(this Direction direction)
+        {
+            return direction switch
+            {
+                Direction.Up => "aim_up",
+                Direction.Down => "aim_down",
+                Direction.Left => "aim_left",
+                Direction.Right => "aim_right",
+                _ => throw new ArgumentOutOfRangeException(nameof(direction), direction,
+                    $"Received not supported direction for aiming {direction}")
+            };
+        }
+    }
+}
---
+++ potions-game/src/Player/PlayerAiming.cs
@@ -0,0 +1,38 @@
+﻿using MonoCustomResourceRegistry;
+using Godot;
+using System;
+using PotionsGame.Core.Extensions;
+using PotionsGame.Core.Utils;
+
+namespace PotionsGame.Player
+{
+    [RegisteredType(nameof(PlayerAiming), baseType = nameof(Node2D))]
+    public class PlayerAiming: Node2D
+    {
+        private Vector2 aimingDirection;
+        private Sprite crosshair;
+        public Subject<bool> IsAiming = new(false);
+        private bool isAiming;
+        
+        public override void _PhysicsProcess(float delta)
+        {
+            aimingDirection = Input.GetVector(
+                Direction.Left.ToAimControlString(),
+                Direction.Right.ToAimControlString(),
+                Direction.Up.ToAimControlString(),
+                Direction.Down.ToAimControlString()
+            );
+
+            isAiming = aimingDirection != Vector2.Zero;
+            IsAiming.Value = isAiming && isAiming != IsAiming.Value; // Notify subscribers only if value changed
+            crosshair.Visible = isAiming;
+            Rotation = aimingDirection.Angle();
+        }
+
+        public override void _Ready()
+        {
+            crosshair = this.GetNodeByTypeOrNull<Sprite>();
+        }
+        
+    }
+}
